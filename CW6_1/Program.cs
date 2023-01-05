b7276a0825e5ee3b1fe40e07279f29995c9950cc
saeid-ya110 <saeidbagheri034@gmail.com>
2023-01-05 16:03:18 +0330

    delete printWelcome method

--- CW6_1/Program.cs
+++ CW6_1/Program.cs
@@ -1,10 +1,4 @@
-﻿void PrintWelcome()
-{
-    Console.WriteLine("Welcome saeid");
-    Console.WriteLine("test");
-    Console.WriteLine("test2");
-}
-
+﻿
 void PrintHello()
 {
     Console.WriteLine("Hello");
